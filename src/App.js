a54dca84fd7912161f970d8fba8246aa55f3b644
rscsam <sam.costley@yahoo.com>
2018-10-20 01:57:18 -0400

    i

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MapContainer from './MapWrapper'
 import './App.css';
+import './arcgis.css'
+import { Map } from 'react-arcgis';
+
+import background from './images/background.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{backgroundImage: `url(${background})`, top:'0', bottom:'0', left:'0', right:'0', position: 'absolute'}}>
+          <h1>Map</h1>
+          <Map />
       </div>
     );
   }
